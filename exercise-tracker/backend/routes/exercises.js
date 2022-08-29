ba0e078ab165b3da6de1386b097b180a5caae309
Frank-dev20 <franknjoe2@gmail.com>
2022-08-29 10:02:14 +0100

    Edited the exercise module

--- exercise-tracker/backend/routes/exercises.js
+++ exercise-tracker/backend/routes/exercises.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const exercise = require('../models/exercise.models');
 let Exercise = require('../models/exercise.models');
 
 router.route('/').get((req, res)=>{
